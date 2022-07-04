ffea81883a6a6e0f55d121f08578bf80ce941b89
fjy666 <97286655+fjy666@users.noreply.github.com>
2022-07-04 16:56:51 +0800

    Removed useless stuff

--- Compare.cpp
+++ Compare.cpp
@@ -1,17 +1,10 @@
-//Code by fjy666
-//luogu-judger-enable-O2
 #include <bits/stdc++.h>
 using namespace std;
 #define _rep(i_,a_,b_) for(int i_ = a_; i_ <= b_; ++i_)
-#define mid ((L+R) >> 1)
-#define get(x) (((x) - 1) / kb + 1)
-#define multiCase() int testCnt = in(); _rep(curCase,1,testCnt)
 typedef long long ll;
 
 int in(void) { int x; scanf("%d", &x); return x; }
 ll inl(void) { ll x; scanf("%lld", &x); return x; }
-template<typename T> void chkmax(T &a, const T &b) { a = max(a, b); } 
-template<typename T> void chkmin(T &a, const T &b) { a = min(a, b); } 
 
 void complie(const char *filename, const char *flag) {
 	string s1 = "g++ ", s2 = ".cpp -Ofast -Wall -Wextra -o ";
@@ -67,12 +60,3 @@ int main(int argc, char** argv) {
 	}
 	return 0;
 }
-
-/* 
-a list of keywords
-clear empty push_back pop_back push pop top front back
-emplace_back emplace push_front pop_front insert erase
-find count set reset bitset map vector string multiset
-first second iterator prev next deque multimap reverse
-sort begin end list modify query init check calc prime 
-*/
